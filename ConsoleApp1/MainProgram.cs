c9bbeb2a8446b7de644d71478d723e919abf72ee
murshed07-copilot <murshed07@outlook.com>
2025-08-23 21:06:34 +0530

    List methods

--- ConsoleApp1/MainProgram.cs
+++ ConsoleApp1/MainProgram.cs
@@ -126,6 +126,7 @@ Console.WriteLine("Hello, World!");
 
 //}
 
+/*
 Employee per1 = new Employee();
 per1.Id = "1";
 per1.FirstName = "Vishnu";
@@ -174,6 +175,155 @@ dept1.Employees.Add(con1);
 
 //employee is assocation to one department
 per2.Department = dept1;
+
+*/
+
+Department department1 = new Department()
+{
+    ID = "1",
+    Name = "IT"
+};
+
+Department department2 = new Department()
+{
+    ID = "2",
+    Name = "HR"
+};
+
+Department department3 = new Department()
+{
+    ID = "3",
+    Name = "Finance"
+};
+
+Employee employee1 = new Employee()
+{
+    Id = "1",
+    FirstName = "A",
+    LastName = "AA",
+    Gender = "M",
+    EmployeeType = EmployeeTypeEnum.Contractor,
+    Department = department1,
+    Salary = 1000
+};
+
+Employee employee2 = new Employee()
+{
+    Id = "2",
+    FirstName = "B",
+    LastName = "BB",
+    Gender = "F",
+    EmployeeType = EmployeeTypeEnum.Permanent,
+    Department = department2,
+    Salary = 1000
+};
+
+
+Employee employee3 = new Employee()
+{
+    Id = "3",
+    FirstName = "C",
+    LastName = "CC",
+    Gender = "M",
+    EmployeeType = EmployeeTypeEnum.Contractor,
+    Department = department3,
+    Salary = 1000
+};
+
+
+Employee employee4 = new Employee()
+{
+    Id = "4",
+    FirstName = "D",
+    LastName = "DD",
+    Gender = "F",
+    EmployeeType = EmployeeTypeEnum.Permanent,
+    Department = department1,
+    Salary = 1400
+};
+
+
+Employee employee5 = new Employee()
+{
+    Id = "5",
+    FirstName = "E",
+    LastName = "EE",
+    Gender = "M",
+    EmployeeType = EmployeeTypeEnum.Contractor,
+    Department = department1,
+    Salary = 1000
+};
+
+
+Employee employee6 = new Employee()
+{
+    Id = "6",
+    FirstName = "F",
+    LastName = "FF",
+    Gender = "M",
+    EmployeeType = EmployeeTypeEnum.Contractor,
+    Department = department2,
+    Salary = 3000
+};
+
+List<Employee> employeeList = new List<Employee>();
+employeeList.Add(employee1);
+employeeList.Add(employee2);
+employeeList.Add(employee3);
+employeeList.Add(employee4);
+employeeList.Add(employee5);
+employeeList.Add(employee6);
+
+//where - all objects
+List<Employee> femaleEmployees = employeeList.Where(emp => emp.Gender == "F").ToList();
+
+//select
+List<string> names = new List<string>();
+foreach (var item in employeeList)
+{
+    names.Add(item.FullName);
+}
+
+names = employeeList.Select(emp => emp.FullName).ToList();
+
+
+//first - 1st female employee = throw exception if no matching records
+Employee firstFemale = employeeList.First(emp => emp.Gender == "F");
+
+List<Employee> femaleEmployees1 = employeeList.Where(emp => emp.Gender == "F").ToList();
+Employee firstFemale1 = femaleEmployees1[0];
+
+//second, third etc.,
+
+//firstordefault => return null if not matching
+Employee firstFemale2 = employeeList.FirstOrDefault(emp => emp.Gender == "F");
+
+//order
+List<Employee> orderedList = employeeList.OrderBy(emp => emp.FullName).ToList(); //ascending
+List<Employee> orderedList1 = employeeList.OrderByDescending(emp => emp.FullName).ToList();
+
+//order by descending using full name and then retrive only the full name, not entire object
+List<string> namesList = employeeList.OrderByDescending(emp => emp.FullName).Select(emp => emp.FullName).ToList();
+
+
+//any - true if one object satisfy the condition
+bool hasFemaleEmployee = employeeList.Any(emp => emp.Gender == "F");
+
+//all - true only if all objects satisfy the condition
+bool isAllFemale = employeeList.All(emp => emp.Gender == "F");
+
+//
+bool hasFemaleEmployee1 = employeeList.Any(emp => emp.Gender == "F" && emp.Id == "2");
+
+//mathematical
+//count, sum, average
+int allEmployeesCount = employeeList.Count();
+int maleEmployeesCount = employeeList.Count(emp => emp.Gender == "M");
+int totalSalary = employeeList.Sum(emp => emp.Salary);
+int totalMaleSalary = employeeList.Where(emp => emp.Gender == "M").Sum(emp => emp.Salary);
+double averageSalary = employeeList.Average(emp => emp.Salary);
+//max, min
+
 
 Console.WriteLine("end");
 
